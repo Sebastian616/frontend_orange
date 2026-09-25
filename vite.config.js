1dda526f442338cb30d0b592eec3244cd446fe41
sebastian <cardonajuanse617@gmail.com>
2026-09-25 15:46:32 -0500

    ruta base2

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Apunta a la raíz del dominio personalizado
-  server: {
-    port: 5173,
-  },
+  base: 'https://www.stororange.lat' // Apunta a la raíz del dominio personalizado
 });
